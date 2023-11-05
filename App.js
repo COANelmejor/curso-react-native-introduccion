6f8b22987d25c8acc6bfdaa739d0e7986a4796ce
Christian O. Arana Navas (COANelmejor) <COANelmejor@gmail.com>
2023-11-04 22:00:36 -0600

    Add NavigationTab and StatusBar to App.js

--- App.js
+++ App.js
@@ -1,13 +1,16 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, StatusBar } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 
 import NavigationStack from './src/navigation/NavigationStack';
-
+import NavigationTab from './src/navigation/NavigationTab';
 
 export default function App() {
   return (
+    
     <NavigationContainer>
-      <NavigationStack />
+      <StatusBar style='auto' />
+      {/* <NavigationStack /> */}
+      <NavigationTab />
     </NavigationContainer>
   );
 }
