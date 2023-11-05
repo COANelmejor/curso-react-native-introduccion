7d9366a10b61fd5938b5af0fc40466868f167ee1
Christian O. Arana Navas (COANelmejor) <COANelmejor@gmail.com>
2023-11-04 21:18:07 -0600

    Agregado react-navigation al proyecto

--- App.js
+++ App.js
@@ -1,21 +1,24 @@
-// import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+
 import LoginForm from './src/components/LoginForm';
 import Saludar from './src/components/Saludar';
 
 export default function App() {
   console.log('App Ejecutado')
   return (
-    <View style={styles.container}>
-      <Text>
-        Curso de React Native
-      </Text>
-      <Saludar />
-      <Saludar firstname="Juan" lastname="Perez" />
-      <Saludar firstname="Maria" lastname="Garcia" />
-      <Saludar />
-      <LoginForm />
-    </View>
+    <NavigationContainer>
+      <View style={styles.container}>
+        <Text>
+          Curso de React Native
+        </Text>
+        <Saludar />
+        <Saludar firstname="Juan" lastname="Perez" />
+        <Saludar firstname="Maria" lastname="Garcia" />
+        <Saludar />
+        <LoginForm />
+      </View>
+    </NavigationContainer>
   );
 }
 
